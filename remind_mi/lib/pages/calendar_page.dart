f00d2be50803812c0133956bf8d4b50650ea9da7
Pierre Vue <83540440+KeeMoh@users.noreply.github.com>
2023-01-26 10:08:41 +0100

    update navCalendar

--- remind_mi/lib/pages/calendar_page.dart
+++ remind_mi/lib/pages/calendar_page.dart
@@ -20,8 +20,11 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  final CalendarController _controller = CalendarController();
   late CalendarView calendarView;
   late ReminderDataSource reminders;
+  Color? _viewHeaderColor = Charter.secondarycolor,
+      _calendarColor = Charter.white;
 
   @override
   void initState() {
@@ -44,66 +47,87 @@ class _CalendarPageState extends State<CalendarPage> {
       ),
       body: Column(
         children: [
+          Expanded(
+            // height: screenWidth,
+            // width: screenWidth,
+            child: SfCalendar(
+              view: CalendarView.month,
+              allowedViews: [
+                // CalendarView.day,
+                CalendarView.week,
+                // CalendarView.workWeek,
+                CalendarView.month,
+                // CalendarView.timelineDay,
+                // CalendarView.timelineWeek,
+                // CalendarView.timelineWorkWeek
+              ],
+              viewHeaderStyle:
+                  ViewHeaderStyle(backgroundColor: _viewHeaderColor),
+              backgroundColor: _calendarColor,
+              controller: _controller,
+              initialDisplayDate: DateTime.now(),
+              dataSource: ReminderDataSource(Reminders.reminders),
+              onTap: calendarTapped,
+              monthViewSettings: const MonthViewSettings(
+                  appointmentDisplayMode:
+                      MonthAppointmentDisplayMode.appointment),
+              // monthViewSettings: MonthViewSettings(
+              //     navigationDirection: MonthNavigationDirection.vertical),
+            ),
+            // dataSource: widget.data["reminders"],
+
+            // // by default the month appointment display mode set as Indicator, we can
+            // // change the display mode as appointment using the appointment display
+            // // mode property
+            // monthViewSettings: const MonthViewSettings(
+            //     appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
+          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        data["calendarView"] = CalendarView.week;
-                        print("setState");
-                        print(data);
-                      });
-                    },
-                    child: Text("Changer de format"))
-              ],
+            child: Center(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          _controller.view = CalendarView.month;
+                        },
+                        child: Text("Mois")),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          _controller.view = CalendarView.week;
+                        },
+                        child: Text("Semaine")),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          _controller.view = CalendarView.day;
+                        },
+                        child: Text("Jour")),
+                  )
+                ],
+              ),
             ),
           ),
-          CalendarMonth(data: data),
         ],
       ),
       floatingActionButton: const AddReminderFloatingButton(),
     );
   }
 
-  void switchCalendarView(view) {
-    // calendarView = view;
-    setState(() {});
+  void calendarTapped(CalendarTapDetails calendarTapDetails) {
+    if (_controller.view == CalendarView.month ||
+        _controller.view == CalendarView.week) {
+      _controller.view = CalendarView.day;
+    }
+    // else if (_controller.view == CalendarView.day) {
+    //   Navigator.of(context).push(FormPage(reminder: calendarTapDetails.targetElement))
+    // }
   }
-
-  // List<Reminder> _getDataSource() {
-  //   final List<Reminder> reminders = <Reminder>[];
-  //   final DateTime today = DateTime.now();
-  //   final DateTime startDate = DateTime(today.year, today.month, today.day, 9);
-  //   final DateTime endDate = startDate.add(const Duration(hours: 2));
-  //   reminders.add(Reminder(
-  //       userID: "", //FirebaseAuth.instance.currentUser!.uid
-  //       title: 'Conference',
-  //       startDate: startDate,
-  //       endDate: endDate));
-  //   return reminders;
-  // }
 }
-
-/// Custom business object class which contains properties to hold the detailed
-/// information about the event data which will be rendered in calendar.
-// class Meeting {
-//   /// Creates a meeting class with required details.
-//   Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
-
-//   /// Event name which is equivalent to subject property of [Appointment].
-//   String eventName;
-
-//   /// From which is equivalent to start time property of [Appointment].
-//   DateTime from;
-
-//   /// To which is equivalent to end time property of [Appointment].
-//   DateTime to;
-
-//   /// Background which is equivalent to color property of [Appointment].
-//   Color background;
-
-//   /// IsAllDay which is equivalent to isAllDay property of [Appointment].
-//   bool isAllDay;
-// }
